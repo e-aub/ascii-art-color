9c68d5b12bd3ab7520fdbc3cb39a7ffbbe248b3f
oamyay <ryuuosamu@outlook.com>
2024-05-16 15:06:57 +0100

    add more handles

--- functions/args.go
+++ functions/args.go
@@ -20,31 +20,34 @@ var OptionsData Options
 func ArgsChecker(args []string) {
 	if len(args) < 2 || len(args) > 5 {
 		fmt.Println("Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
-		os.Exit(1)
+		os.Exit(0)
 	}
 
-	colorPattern := regexp.MustCompile(`^--color=`)
+	colorPattern := regexp.MustCompile(`^--color`)
 	colorCheck := regexp.MustCompile(`^(?:--color=)(\w+)`)
 
-	outputPattern := regexp.MustCompile(`^--output=`)
+	outputPattern := regexp.MustCompile(`^--output`)
 	outputCheck := regexp.MustCompile(`^(?:--output=)(\w+\W*\w*\.txt)$`)
 
 	if len(args) == 2 {
+		if colorPattern.MatchString(args[1]) || outputPattern.MatchString(args[1]) {
+			fmt.Println("Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
+			os.Exit(0)
+		}
 		OptionsData.Input = args[1]
-		fmt.Println(OptionsData)
 		return
 	}
 
-	if match := colorPattern.FindStringSubmatch(args[1]); match != nil {
+	if colorPattern.MatchString(args[1]) {
 		if len(args) < 4 {
 			fmt.Println("Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
-			os.Exit(1)
+			os.Exit(0)
 		}
 
 		color := colorCheck.FindStringSubmatch(args[1])
 		if color == nil {
 			fmt.Println("Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
-			os.Exit(1)
+			os.Exit(0)
 		}
 
 		OptionsData.Color = color[1]
@@ -56,19 +59,19 @@ func ArgsChecker(args []string) {
 		} else {
 			OptionsData.Banner = "standard"
 		}
-	} else if match := outputPattern.FindStringSubmatch(args[1]); match != nil {
+	} else if outputPattern.MatchString(args[1]) {
 		if len(args) < 3 {
 			fmt.Println("Usage: go run . [OPTION] [STRING]\n\nEX: go run . --output=<filename.txt> \"something\"")
-			os.Exit(1)
+			os.Exit(0)
 		}
 
 		output := outputCheck.FindStringSubmatch(args[1])
 		if output == nil {
 			fmt.Println("Usage: go run . [OPTION] [STRING]\n\nEX: go run . --output=<filename.txt> \"something\"")
-			os.Exit(1)
+			os.Exit(0)
 		}
 
-		OptionsData.OutputFile = match[1]
+		OptionsData.OutputFile = output[1]
 		OptionsData.Input = args[2]
 
 		if len(args) == 4 {
